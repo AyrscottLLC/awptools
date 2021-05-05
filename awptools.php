1509526c2729bb8a260efd151dd432016162a2d4
Jared <jareddeblander@gmail.com>
2021-05-05 18:38:32 -0400

    Update awptools.php

--- awptools.php
+++ awptools.php
@@ -1,4 +1,7 @@
 <?php
 /**
-*
+* Plugin Name:      Ayrscott WordPres Tools
+* Author:           Jared De Blander, Ayrscott LLC
+* Author URI:       https://ayrscott.com/
+* Description:      Our base WordPress library to fork new projects from.
 */
