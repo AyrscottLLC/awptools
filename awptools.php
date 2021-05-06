514ca67022fdf4475b81db1e8b3637424fc993bd
Jared <jareddeblander@gmail.com>
2021-05-06 09:55:39 -0400

    Update awptools.php

--- awptools.php
+++ awptools.php
@@ -6,8 +6,21 @@
 * Description:      Our base WordPress library to fork new projects from.
 */
 
+
+// Check for a constant set by WordPress to ensure we are not being access directly
+if ( ! defined('ABSPATH') ) {
+  exit;
+}
+
+
+// define locations of both this file on the filesystem and the URL path to this file
+define('AWPTOOLS_LOCATION_FILE', dirname(__FILE__));
+define('AWPTOOLS_LOCATION_URL',  plugins_url('', __FILE__));
+
 // Conditional loading of admin code is performed here:
 if ( is_admin() ) {
   // we are in admin mode
-  // require_once __DIR__ . '/admin/plugin-name-admin.php';
+  
+  // sample include file
+  // require_once __DIR__ . '/admin/plugin-name-admin.php'; 
 }
